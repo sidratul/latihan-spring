05b94d9011dc14e901a6d9d697202e93a9f72f2f
sidratul <muhammad.mochi@gmail.com>
2013-08-28 05:40:56 +0700

    tambah pom.xml + implements method PesonDaoSpring

--- src/main/java/turotorial/spring/dao/springjdbc/PersonDaoSpring.java
+++ src/main/java/turotorial/spring/dao/springjdbc/PersonDaoSpring.java
@@ -1,5 +1,26 @@
 package turotorial.spring.dao.springjdbc;
 
-//public class PersonDaoSpring implements PersonDao{
-//    
-//}
+import java.util.List;
+import org.springframework.stereotype.Repository;
+import turotorial.spring.dao.PersonDao;
+import turotorial.spring.model.Person;
+
+@Repository("personDao")
+public class PersonDaoSpring implements PersonDao{
+    
+    @Override
+    public List<Person> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public Person getById(long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public void save(Person p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
