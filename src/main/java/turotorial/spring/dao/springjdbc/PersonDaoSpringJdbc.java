d92dbd563539ef666b0c08d89ee11bbad2cfd9e9
sidratul <muhammad.mochi@gmail.com>
2013-08-28 09:02:45 +0700

    setter getter Person.java

--- src/main/java/turotorial/spring/dao/springjdbc/PersonDaoSpringJdbc.java
+++ src/main/java/turotorial/spring/dao/springjdbc/PersonDaoSpringJdbc.java
@@ -1,8 +1,12 @@
 package turotorial.spring.dao.springjdbc;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +20,23 @@ public class PersonDaoSpringJdbc implements PersonDao{
     private static final Log LOG = LogFactory.getLog(PersonDaoSpringJdbc.class);
     
     private SimpleJdbcTemplate simpleJdbcTemplate;
-    private 
+    private SimpleJdbcInsert simpleJdbcInsert;
+    
+    private static final String SQL_GET_ALL = "select * from T_PERSON";
+    private static final String SQL_GET_BY_ID = "select * from T_PERSON where id=?";
+    private static final String SQL_UPDATE = "UPDATE T_PERSON set name=? , email=? where id=?";
+    
+    private static final class PersonMapper implements 
+            ParameterizedRowMapper<Person>{
+        
+        @Override
+        public Person mapRow(final ResultSet rs,final int rowNum) 
+                throws SQLException {
+            final Person result = new Person();
+            //result.setId
+        }
+    
+    }
     
     @Override
     public List<Person> getAll() {
